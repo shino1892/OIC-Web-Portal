b59a391cdbf975bfd9df59002cf556a689e419a4
shino <24110016@oic-ok.ac.jp>
2025-11-27 09:23:00 +0900

    DB処理変更

--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -2,20 +2,9 @@ from flask import Flask, request, jsonify
 from google.oauth2 import id_token
 from google.auth.transport import requests as grequests
 from flask import Blueprint, jsonify
-from flask_cors import CORS
-from app.utility.db.db_access import db_connect
+from app.utility.db.db_test import get_departments
 
 user_bp = Blueprint('user', __name__)
-# CORS(user_bp, origins=["http://localhost:3000","http://localhost"], supports_credentials=True)
-
-# @user_bp.after_request
-# def add_cors_headers(response):
-#     response.headers.add("Access-Control-Allow-Origin", "http://localhost:3000")
-#     response.headers.add("Access-Control-Allow-Origin", "http://localhost")
-#     response.headers.add("Access-Control-Allow-Credentials", "true")
-#     response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
-#     response.headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-#     return response
 
 @user_bp.route("/auth/google", methods=["POST"])
 def google_login():
@@ -38,10 +27,8 @@ def google_login():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
     
-@user_bp.route("/get/departments", methods=["POST"])
-def get_departments():
-    rows = db_connect()
-
-    print(rows)
+@user_bp.route("/get/db_test", methods=["POST"])
+def db_test():
+    rows = get_departments()
 
     return jsonify({"departments": rows}), 200
