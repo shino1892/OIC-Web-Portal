a3c08eb9eb0908ed94d4089f234fca1703ae5680
h-sanda <24110001@oic-ok.ac.jp>
2025-11-28 01:20:08 +0900

    トップページデザインー三田

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,100 @@
-// frontend/app/page.tsx
 "use client";
+import React from "react";
+import "./globals.css";
+
+// frontend/app/page.tsx
 // import { useEffect, useState } from "react";
 // import { Button } from "@/components/ui/Button";
 
 export default function Home() {
   return (
     <main>
+      <nav className="flex gap-8 text-2xl h-16 shadow font-bold items-center justify-center bg-blue-300">
+        <button className="hover:text-blue-600">時間割</button>
+        <button className="hover:text-blue-600">出席管理</button>
+        <button className="hover:text-blue-600">実習課題管理</button>
+        <button className="hover:text-blue-600">イベント情報</button>
+        <button className="hover:text-blue-600">キャリア支援</button>
+        <button className="hover:text-blue-600">コミュニティ</button>
+        <button className="hover:text-blue-600">AI学習支援</button>
+      </nav>
+
+
+
+
+      <br></br>
       <h1>Welcom to OIC!!</h1>
-      <button>時間割</button>
-      <button>出席管理</button>
-      <button>実習課題管理</button>
-      <button>イベント情報</button>
-      <button>キャリア支援</button>
-      <button>コミュニティ</button>
-      <button>AI学習支援</button>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
+      <br></br>
+      <p>改行テスト</p>
     </main>
   );
 }
