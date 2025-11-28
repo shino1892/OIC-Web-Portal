0f0a7b2f028ce2b4591ee1215587b3ebb28dce1f
Okamoto.S <24110014@oic-ok.ac.jp>
2025-11-28 14:22:13 +0900

    add AIサポートページ追加

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,18 +2,19 @@
 "use client";
 // import { useEffect, useState } from "react";
 // import { Button } from "@/components/ui/Button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1>Welcom to OIC!!</h1>
-      <button>時間割</button>
-      <button>出席管理</button>
-      <button>実習課題管理</button>
-      <button>イベント情報</button>
-      <button>キャリア支援</button>
-      <button>コミュニティ</button>
-      <button>AI学習支援</button>
+      <Link href="/time-table"><button>時間割</button></Link>
+      <Link href="/attendance"><button>出席管理</button></Link>
+      <Link href="/tasks"><button>実習課題管理</button></Link>
+      <Link href="/events"><button>イベント情報</button></Link>
+      <Link href="/career"><button>キャリア支援</button></Link>
+      <Link href="/community"><button>コミュニティ</button></Link>
+      <Link href="/ai-support"><button>AI学習支援</button></Link>
     </main>
   );
 }
