e4ea3ae32fdd6b8853cace6aec0b36b56f82020c
shino <24110016@oic-ok.ac.jp>
2025-12-04 08:34:34 +0900

    update 認証用のfetchフックを追加し、関連するファイルを.gitignoreに追加

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,6 @@ import "./globals.css";
 // frontend/app/page.tsx
 // import { useEffect, useState } from "react";
 // import { Button } from "@/components/ui/Button";
-import Link from "next/link";
 
 export default function Home() {
   return (
