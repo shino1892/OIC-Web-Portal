9a3eddf4f4874fc199efe5b677e8f794aca3af14
kona- <24110024@oic-ok.ac.jp>
2025-12-12 10:49:47 +0900

    add ホームコンポーネントを追加し、ページに表示するように修正。package.jsonのdevスクリプトを更新し、不要なpeerフィールドを削除。

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React from "react";
+import App from  "../components/home";
+import ReactDOM from "react-dom/client";
 import "./globals.css";
 
 // frontend/app/page.tsx
@@ -10,6 +12,8 @@ export default function Home() {
   return (
     <main>
       <br />
+      <App name="ReactTS" />
     </main>
   );
 }
+
---
+++ frontend/src/components/home.tsx
@@ -0,0 +1,8 @@
+
+type Props = {
+  name: string;
+};
+
+export default function App({ name }: Props) {
+  return <h1>Hello {name}!</h1>;
+}   
